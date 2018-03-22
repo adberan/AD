0face711a4854138149c89f4e264fb299464f2a8
adberan <37648789+adberan@users.noreply.github.com>
2018-03-22 12:17:42 +0700

    300x250

---
+++ 300x250.js
@@ -0,0 +1,20 @@
+var arldgzn_id = 1;
+var arldgzn_now = new Date()
+var detik = arldgzn_now.getSeconds()
+var arldgzn_place = detik % arldgzn_id;
+arldgzn_place += 1;
+if (arldgzn_place == 1) {
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- 300x250 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:250px"
+     data-ad-client="ca-pub-1752291776520492"
+     data-ad-slot="8979277681"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+}
+document.write('<a href=\"' + url + '\" target=\"_blank\" rel=\"nofollow\">');
+document.write('<img src=\"' + banner + '\" width=')
+document.write(width + ' height=' + height + ' ');
+document.write('title=\"' + title + '\" alt=\"' + alt + '\"></a>');

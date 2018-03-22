1838fe1d05fa5b9e3926db1936138f3ca0775c30
adberan <37648789+adberan@users.noreply.github.com>
2018-03-22 13:50:36 +0700

    Update 300x250.js

--- 300x250.js
+++ 300x250.js
@@ -5,7 +5,5 @@ document.writeln(" <ins class=\"adsbygoogle\" ");
 document.writeln("      style=\"display:inline-block;width:300px;height:250px\" "); 
 document.writeln("      data-ad-client=\"ca-pub-1752291776520492\" "); 
 document.writeln("      data-ad-slot=\"8979277681\"></ins> "); 
-document.writeln(" <script> "); 
-document.writeln(" (adsbygoogle = window.adsbygoogle || []).push({}); "); 
-document.writeln(" </script> ");
+document.writeln("  ");
  // -->

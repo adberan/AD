fef569b1145ec331c2583990ec257390b496cb23
adberan <37648789+adberan@users.noreply.github.com>
2018-03-23 19:53:10 +0700

    Update 300x250.js

--- 300x250.js
+++ 300x250.js
@@ -1,4 +1,9 @@
-google_ad_client = "ca-pub-1752291776520492";
+    google_ad_client = "ca-pub-1752291776520492";
     google_ad_slot = "8979277681";
     google_ad_width = 300;
     google_ad_height = 250;
+info="<script type=&#34;text/javascript&#34;
+src=&#34;//pagead2.googlesyndication.com/pagead/show_ads.js&#34;>
+</script>"
+
+document.write(info)

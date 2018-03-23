39ba1bf5185d0b4de60c93189d97de91b17383dd
adberan <37648789+adberan@users.noreply.github.com>
2018-03-23 20:12:52 +0700

    Update and rename f.js to 728x90.js

---
+++ 728x90.js
@@ -0,0 +1,7 @@
+    google_ad_client = "ca-pub-1752291776520492";
+    google_ad_slot = "3707318447";
+    google_ad_width = 728;
+    google_ad_height = 90;
+document.write( '<script type=\"text/javascript\"\n' );
+document.write( 'src=\"//pagead2.googlesyndication.com/pagead/show_ads.js\">\n' );
+document.write( '</script>' );

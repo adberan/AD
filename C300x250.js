8afdd308b767e7ccf53d3df5a17e252edf010f32
adberan <37648789+adberan@users.noreply.github.com>
2018-03-24 01:36:53 +0700

    Create C300x250.js

---
+++ C300x250.js
@@ -0,0 +1,1 @@
+document.write( '<iframe src=\'//ap.adberan.com/p/300x250.html\' scrolling=\'no\' style=\'width:728px; height:90px; border:0px; padding:0;overflow:hidden\' allowtransparency=\'true\'></iframe>' );

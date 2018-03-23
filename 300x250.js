34c11f1cf18dc16184c8eb66c0aca55fc49e2dab
adberan <37648789+adberan@users.noreply.github.com>
2018-03-23 19:55:21 +0700

    Update 300x250.js

--- 300x250.js
+++ 300x250.js
@@ -2,8 +2,6 @@
     google_ad_slot = "8979277681";
     google_ad_width = 300;
     google_ad_height = 250;
-info="<script type=&#34;text/javascript&#34;
-src=&#34;//pagead2.googlesyndication.com/pagead/show_ads.js&#34;>
-</script>"
-
-document.write(info)
+document.write( '<script type=\"text/javascript\"\n' );
+document.write( 'src=\"//pagead2.googlesyndication.com/pagead/show_ads.js\">\n' );
+document.write( '</script>' );
